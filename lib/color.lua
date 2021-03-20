066f0265dee5023b5f71fc80b828c5d515f167d6
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-03-20 13:01:24 -0500

    e

--- lib/color.lua
+++ lib/color.lua
@@ -1,10 +1,10 @@
 
 local color =
 {
-    black   = "0x000000",
-    white   = "0xFFFFFF",
-    red     = "0xFF0000",
-    green   = "0x00FF00",
-    blue    = "0x0000FF",
+    black   = 0x000000,
+    white   = 0xFFFFFF,
+    red     = 0xFF0000,
+    green   = 0x00FF00,
+    blue    = 0x0000FF
 }
 return color
--- lib/graphics.lua
+++ lib/graphics.lua
@@ -11,9 +11,9 @@ graphics.set_gpu = function(self, gpu)
     if type(gpu) == "string" then
         _gpuAddr = gpu
     elseif type(gpu) == "table" then
-        _gpuAddr = component.proxy(gpu.address)
+        _gpuAddr = gpu.address
     else
-        _gpuAddr = component.proxy(component.list("gpu")())
+        _gpuAddr = component.list("gpu")()
     end
 
     if _gpuAddr then
@@ -26,21 +26,21 @@ end
 
 -- Draws a rectangle on the screen
 graphics.rect = function(self, x, y, width, height, color)
-    local gpu = self.gpu
-    assert(gpu)
+    local _gpu = self.gpu
+    assert(_gpu)
 
-    local _oldForeground = gpu.getForeground()
+    local _oldForeground = _gpu.getForeground()
 
-    local _col = color or "0xFFFFFF"
-    gpu.setForeground(_col)
-    gpu.fill(x, y, width, height, " ")
-    gpu.setForeground(_oldForeground)
+    local _col = color or 0xFFFFFF
+    _gpu.setForeground(_col)
+    _gpu.fill(x, y, width, height, " ")
+    _gpu.setForeground(_oldForeground)
 end
 
 -- Clears screen
 graphics.clear = function(self, color)
     local _w, _h = self.gpu.getResolution()
-    local _col = color or "0x000000"
+    local _col = color or 0x000000
     self:rect(1, 1, _w, _h, _col)
 end
 
